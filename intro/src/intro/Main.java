b90a487d674de50db05743b1dbeb883ae7d12566
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-04 15:35:30 +0300

    Add difference between data and variable

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        String text = "foo";
+
+        System.out.println(text);   // will print "foo"
+        System.out.println("text"); // will print "text"
     }
 }
