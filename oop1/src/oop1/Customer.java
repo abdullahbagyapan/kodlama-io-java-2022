049cb608e747b83079343879c67a37602c2d8927
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-04 16:55:27 +0300

    Create new class 'Customer.java'

---
+++ oop1/src/oop1/Customer.java
@@ -0,0 +1,22 @@
+package oop1;
+
+public class Customer {
+    private int id;
+    private String phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
