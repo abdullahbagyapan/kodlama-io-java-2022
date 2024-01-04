d00cd25ba233ed84b5b97c00839161e96e2a3de8
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-04 16:17:22 +0300

    Create new 'Product' object and set values in 'Main.java'

--- oop1/src/oop1/Main.java
+++ oop1/src/oop1/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         
+        Product product = new Product();
+
+        product.name = "Lenovo Thinkpad";
+        product.unitPrice = 350;
+        product.discount = 10;
+        product.unitsInStock = 50;
+        product.imageUrl = "productImage/lenovo-thinkpad";
+
+        
     }
     
 }
