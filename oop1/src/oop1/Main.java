14cc5ca9762a1599a4034c92097d95412cd756a0
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-04 17:56:48 +0300

    Create new 'IndividualCustomer' and 'CorporateCustomer' object in 'Main.java'

--- oop1/src/oop1/Main.java
+++ oop1/src/oop1/Main.java
@@ -12,7 +12,20 @@ public class Main {
         product.setUnitsInStock(50);
         product.setImageUrl("productImage/lenovo-thinkpad");
 
+        IndividualCustomer individualCustomer = new IndividualCustomer();
+        individualCustomer.setId(1);
+        individualCustomer.setName("Customer");
+        individualCustomer.setPhoneNumber("0300");
 
+        CorporateCustomer corporateCustomer = new CorporateCustomer();
+        corporateCustomer.setCompanyName("Lenovo");
+        corporateCustomer.setId(1);
+        corporateCustomer.setPhoneNumber("0301");
+        corporateCustomer.setTaxNumber("1111111");
+
+
+        Customer[] customers = {individualCustomer,corporateCustomer};
+        
     }
     
 }
