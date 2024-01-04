e8f6ae5dc438706ad710a52b81f9e628ceaab850
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-04 16:26:48 +0300

    Create getter and setter methods for 'Product.java'

--- oop1/src/oop1/Product.java
+++ oop1/src/oop1/Product.java
@@ -8,4 +8,47 @@ public class Product {
     private double unitPrice;
     private double discount;
 
+        // Getter methods
+        public String getName() {
+            return name;
+        }
+    
+        public String getImageUrl() {
+            return imageUrl;
+        }
+    
+        public Integer getUnitsInStock() {
+            return unitsInStock;
+        }
+    
+        public double getUnitPrice() {
+            return unitPrice;
+        }
+    
+        public double getDiscount() {
+            return discount;
+        }
+    
+        // Setter methods
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public void setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+    
+        public void setUnitsInStock(Integer unitsInStock) {
+            this.unitsInStock = unitsInStock;
+        }
+    
+        public void setUnitPrice(double unitPrice) {
+            this.unitPrice = unitPrice;
+        }
+    
+        public void setDiscount(double discount) {
+            this.discount = discount;
+        }
+    
+
 }
