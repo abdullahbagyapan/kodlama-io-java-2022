72d7033b77ca39163a1190c12bbc3750a8c4a991
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-04 16:58:34 +0300

    Create new class 'IndividualCustomer.java'

---
+++ oop1/src/oop1/IndividualCustomer.java
@@ -0,0 +1,16 @@
+package oop1;
+
+public class IndividualCustomer extends Customer{
+
+    private String name;
+
+    public String setName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
