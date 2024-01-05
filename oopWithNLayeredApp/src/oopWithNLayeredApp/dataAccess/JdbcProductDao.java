878fa2fec546b760980844b17079b01a5e3e0f70
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-05 15:16:04 +0300

    Implement 'ProductDaoI.java' interface for 'JdbcProductDao.java' class

--- oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/JdbcProductDao.java
@@ -2,7 +2,7 @@ package oopWithNLayeredApp.dataAccess;
 
 import oopWithNLayeredApp.entities.Product;
 
-public class JdbcProductDao {
+public class JdbcProductDao implements ProductDaoI{
 
     public void insert(Product product) {
         System.out.println("Inserted to DB by JDBC");
