ff5d1168fb9d2224ca56b68b048642867897ce4a
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-05 14:43:38 +0300

    Create 'Product' and 'ProductManager' object in 'Main.java'

--- oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,7 +1,16 @@
 package oopWithNLayeredApp;
 
+import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.entities.Product;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+
+        Product product = new Product(1,"Iphone X",870.90);
+
+        ProductManager productManager = new ProductManager();
+        productManager.insert(product);
 
         
     }
