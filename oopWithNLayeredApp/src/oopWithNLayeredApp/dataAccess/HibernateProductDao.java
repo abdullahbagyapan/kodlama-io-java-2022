f9c292cc5dda6197d3ff97d63a72571fa8bacd93
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-05 15:18:50 +0300

    Implement 'ProductDaoI.java' interface for 'HibernateProductDao.java' class

--- oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -2,9 +2,8 @@ package oopWithNLayeredApp.dataAccess;
 
 import oopWithNLayeredApp.entities.Product;
 
-public class HibernateProductDao {
+public class HibernateProductDao implements ProductDaoI{
     
-
         public void insert(Product product) {
         System.out.println("Inserted to DB by Hibernate");
     }
