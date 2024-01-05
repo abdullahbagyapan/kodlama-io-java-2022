688cb49be69ca81b880c671a0a2c8d9ea14b3acb
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-01-05 15:36:13 +0300

    Feat: Add loose dependency to 'ProductManager.java'
    -Add loose dependency between 'ProductDaoI' and 'ProductManager.java'

--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,10 +1,15 @@
 package oopWithNLayeredApp.business;
 
-import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.ProductDaoI;
 import oopWithNLayeredApp.entities.Product;
 
 public class ProductManager {
+
+    private ProductDaoI productDao;
+    // loose dependency
+    public ProductManager(ProductDaoI productDao) {
+        this.productDao = productDao;
+    }
 
     public void insert(Product product) {
         
@@ -13,7 +18,6 @@ public class ProductManager {
             throw new IllegalArgumentException("The unit price cannot be less than 10");
         }
 
-        ProductDaoI productDao = new HibernateProductDao();
         productDao.insert(product);
     }
     
